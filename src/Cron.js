45177b1a1d3fa492708644b4702ff14f91cb35cb
Tycho Bokdam <tycho@palmtreecoding.com>
2020-04-21 07:59:30 +0200

    feat: Added schedule to cron job
    So the job can be scheduled without triggering it imeditialy

--- src/Cron.js
+++ src/Cron.js
@@ -19,11 +19,13 @@ export default class Cron {
    * @param {!Object} [options={}] - The options for the Cron middleware.
    * @param {!string} [options.cronTime=0 0 *\/6 * * *] - The cron tab to
    * execute the scraper.
-   * @param {?boolean} [options.start=false] - Start the cronjob on creation.
+   * @param {?boolean} [options.start=false] - Start the cron job on creation.
+   * @param {?boolean} [options.schedule=true] - Should the cron job be scheduled.
    */
   constructor(PopApi: any, {
     cronTime = '0 0 */6 * * *',
     start = false,
+    schedule = true,
   }: Object = {}): void {
     const { name } = this.constructor
     PopApi.debug(`Registering ${name} with options: %o`, {
@@ -36,16 +38,17 @@ export default class Cron {
      * @type {string}
      */
     this.cronTime = cronTime
-    PopApi.cron = this.getCron(PopApi, start)
+    PopApi.cron = this.getCron(PopApi, start, schedule)
   }
 
   /**
    * Get the cron job to run.
    * @param {!PopApi} PopApi - The PopApi instance.
    * @param {?boolean} [start] - Start the cron job.
+   * @param {?boolean} [schedule] - Schedule the cron job.
    * @returns {Object} - A configured cron job.
    */
-  getCron(PopApi: any, start?: boolean): Object {
+  getCron(PopApi: any, start?: boolean, schedule?: boolean): Object {
     if (start) {
       PopApi.scraper.scrape()
     }
@@ -54,7 +57,7 @@ export default class Cron {
       this.cronTime,
       PopApi.scraper.scrape,
       {
-        scheduled: start,
+        scheduled: schedule,
       },
     )
   }
